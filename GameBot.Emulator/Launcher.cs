e6c1b95dc75655f5e8f4c1c86b27a702b9394bf4
winki <mathiaswinkler89@gmail.com>
2016-02-01 23:07:55 +0100

    initial commit

---
+++ GameBot.Emulator/Launcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBot.Emulator
+{
+    public class Launcher
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+        }
+    }
+}
